f4dc4baba71cd50b4803a45defbd41e51a8bf399
kentarolrt <kentaroloret1@gmail.com>
2026-04-10 01:02:59 +0200

    add js and css

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import flask
+from flask import url_for
 
 app = flask.Flask(__name__, static_folder = 'static', static_url_path = '/static')
 
 @app.route('/')
 def home():
-
+    a = url_for('static', filename='html')
     return flask.send_file('./static/html/index.html')
+
+@app.post('/signup')
+def signup():
+
+    return flask.send_file('./static/html/signup.html')
 
 app.run(port = 5500)
