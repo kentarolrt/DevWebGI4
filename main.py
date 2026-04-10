4b49130c7ffebac642a3482c330d35b6b267f2ff
kentarolrt <kentaroloret1@gmail.com>
2026-04-10 13:22:31 +0200

    add login route

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import flask
-from flask import url_for
 
 app = flask.Flask(__name__, static_folder = 'static', static_url_path = '/static')
 
 @app.route('/')
 def home():
-    a = url_for('static', filename='html')
     return flask.send_file('./static/html/index.html')
 
-@app.post('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
 
     return flask.send_file('./static/html/signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    
+    return flask.send_file('./static/html/login.html')
 
 app.run(port = 5500)
